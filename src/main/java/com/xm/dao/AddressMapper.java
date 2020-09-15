3b445b989ced425678e1be5296d3924bfed38d97
李超 <707132922@qq.com>
2020-09-15 10:08:11 +0800

    小修改1.0

--- src/main/java/com/xm/dao/AddressMapper.java
+++ src/main/java/com/xm/dao/AddressMapper.java
@@ -1,6 +1,7 @@
 package com.xm.dao;
 
 import com.xm.entity.Address;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ import java.util.List;
  * @date 2020/9/14 - 15:26
  * @function
  */
+@Repository
 public interface AddressMapper {
 
     public List<Address> getAllAddress();
